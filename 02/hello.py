53164d10cce62f9c2989396a2758adc08a7732c2
tokhy <dev.tokhy@gmail.com>
2023-02-27 20:37:33 +0200

    Add Variables

--- 02/hello.py
+++ 02/hello.py
@@ -1,1 +1,10 @@
 print("hello python")
+#--------------------#
+# Variables
+i = 5                   # int
+f = 5.2                 # float
+s = "this is a string"  # string
+b = True or False       # Bool
+
+type_of = type(b)       # get typeof var
+print(type_of)
